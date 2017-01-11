581556b51357d54cd4ebb295389c8790970e1174
Anant Shrivastava <anant@anantshri.info>
2017-01-12 00:54:09 +0530

    now reporting vulnerabilities picked by shodan too. such as heartbleed

--- shodan_hostenum.py
+++ shodan_hostenum.py
@@ -12,25 +12,34 @@ $python shodan_ip.py 8.8.8.8 2>/dev/null
         Operating System: None
 
 Port: 53 ;
-		Banner: 1a648183000100000001000002383303323133033136340331313207696e2d61646472046172706100000c0001c0130006000100000707002f016703646e73026b720007696e7665727365036e6963026f72c03f77fd2c19000054600000038400093a800000a8c0
+        Banner: 1a648183000100000001000002383303323133033136340331313207696e2d61646472046172706100000c0001c0130006000100000707002f016703646e73026b720007696e7665727365036e6963026f72c03f77fd2c19000054600000038400093a800000a8c0
 '''
 #
 import sys
 import shodan
 api = shodan.Shodan('API_KEY_HERE')
 if len(sys.argv) > 1:
-	#print "Lets check api"
-	host = api.host(sys.argv[1])
+    #print "Lets check api"
+    host = api.host(sys.argv[1])
 
-	# Print general info
-	print """
+    # Print general info
+    print """
         IP: %s
         Organization: %s
         Operating System: %s
-	""" % (host['ip_str'], host.get('org', 'n/a'), host.get('os', 'n/a'))
-	# Print all banners
-	for item in host['data']:
-	        print """Port: %s ;
-		Banner: %s""" % (item['port'], item['data'])
+    """ % (host['ip_str'], host.get('org', 'n/a'), host.get('os', 'n/a'))
+    # Print all banners
+    # Print all Vuln's caught by Shodan
+    print "Vulnerabilities: "
+    for vuln in host['vulns']:
+        if vuln.find("!") == -1:
+            vulncnt = vulncnt + 1
+            print "**  " + vuln + "  **"
+    if vulncnt == 0:
+        print "No Vulnerability picked by Shodan"
+    print ""
+    for item in host['data']:
+            print """Port: %s ;
+        Banner: %s""" % (item['port'], item['data'])
 else:
-	exit
+    exit
