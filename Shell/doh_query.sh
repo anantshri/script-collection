ffb4262539ed44a2520c43b4ac8b144b25aca2a4
Anant Shrivastava <anant.shrivastava@gmail.com>
2024-10-21 23:12:08 +0530

    improved the code

--- Shell/doh_query.sh
+++ Shell/doh_query.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
+# Script by Anant Shrivastava (https://anantshri.info)
 echo "Script to validate DoH Queries"
-print_header() {
-    printf "%-15s %-15s %-25s\n" "Provider" "DNS IP" "Resolved data"
-    echo "---------------------------------------------------------"
-}
-print_row() {
-    printf "%-15s %-15s %-25s\n" "$1" "$2" "$3"
-}
-if [ $# -eq 1 ]
-then
-	name=$1
-	echo "DNS Resolution for : $name"
-	echo ""
-	print_header
-	print_row "CloudFlare" "1.1.1.1" $(curl -q --http2 --header "accept: application/dns-json" "https://cloudflare-dns.com/dns-query?name=$name" 2>/dev/null | jq '.Answer[]?.data' -r)
-	print_row "Quad 9" "9.9.9.9" $(curl -q --http2 --header "accept: application/dns-json" "https://dns.quad9.net:5053/dns-query?name=$name" 2>/dev/null | jq '.Answer[]?.data' -r)
-	print_row "Google DNS" "8.8.8.8" $(curl -q --http2 --header "accept: application/dns-json" "https://dns.google/resolve?name=$name" 2>/dev/null | jq '.Answer[]?.data' -r)
-	print_row "DNS0.eu" "" $(curl -q --http2 --header "accept: application/dns-json" "https://dns0.eu/dns-query?name=$name" 2>/dev/null | jq '.Answer[]?.data' -r)
 
+print_header() {
+    printf "%-15s %-25s\n" "Provider" "Resolved Data"
+    echo "----------------------------------------------"
+}
+
+print_row() {
+    printf "%-15s %-25s\n" "$1" "$2"
+}
+
+# Define an array of providers with their details
+providers=(
+    "CloudFlare|https://cloudflare-dns.com/dns-query?name={domain}"
+    "Quad9|https://dns.quad9.net:5053/dns-query?name={domain}"
+    "GoogleDNS|https://dns.google/resolve?name={domain}"
+    "DNS0.eu|https://dns0.eu/dns-query?name={domain}"
+    # Add more providers as needed
+
+)
+
+
+if [ $# -eq 1 ]; then
+    domain=$1
+    echo "DNS Resolution for: $domain"
+    echo ""
+    print_header
+
+
+    for provider in "${providers[@]}"; do
+        IFS='|' read -r name url_template <<< "$provider"
+        url="${url_template//\{domain\}/$domain}"
+
+        # Fetch and parse the DNS response
+        response=$(curl -s --http2 --header "accept: application/dns-json" "$url")
+        resolved_data=$(echo "$response" | jq -r '.Answer[]?.data' | paste -sd "," -)
+
+        print_row "$name" "$resolved_data"
+    done
 else
-	echo "$0 <domain name>"
+    echo "Usage: $0 <domain name>"
 fi
